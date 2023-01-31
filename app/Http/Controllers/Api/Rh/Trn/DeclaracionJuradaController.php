22922c13a1d5ed187745bbd3221d985dc231f570
coroneldev <rcoroneldeveloper@gmail.com>
2023-01-31 15:23:54 -0400

    Controlador de Declaraciones Juaradas

--- app/Http/Controllers/Api/Rh/Trn/DeclaracionJuradaController.php
+++ app/Http/Controllers/Api/Rh/Trn/DeclaracionJuradaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Rh\Trn;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\DeclaracionJurada;
 
 class DeclaracionJuradaController extends Controller
 {
@@ -14,7 +15,13 @@ class DeclaracionJuradaController extends Controller
      */
     public function index()
     {
-        //
+        $declaracionesJuradas = DeclaracionJurada::all();
+
+        return response()->json([
+            'status'    => true,
+            'message'   => 'Registro de declaraciones juradas recuperadas exitosamente',
+            'data'      => $declaracionesJuradas
+        ], 200);
     }
 
     /**
@@ -25,7 +32,19 @@ class DeclaracionJuradaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $declaracionJurada = new DeclaracionJurada();
+        $declaracionJurada->persona_id                       = $request->persona_id;
+        $declaracionJurada->declaracion_jurada               = $request->declaracion_jurada;
+        $declaracionJurada->fecha_inicio                     = $request->fecha_inicio;
+        $declaracionJurada->fecha_fin                        = $request->fecha_fin;
+        $declaracionJurada->vigente                          = $request->vigente;
+        $declaracionJurada->save();
+
+        return response()->json([
+            'status'    => true,
+            'message'   => 'Registro creado exitosamente',
+            'data'      => $declaracionJurada
+        ], 201);
     }
 
     /**
@@ -36,7 +55,20 @@ class DeclaracionJuradaController extends Controller
      */
     public function show($id)
     {
-        //
+        $declaracionJurada = DeclaracionJurada::find($id);
+        
+        if (is_null($declaracionJurada)) {
+            return response()->json([
+                'status'    => false,
+                'message'   => 'Solicitud de registro no encontrado'
+            ], 200);
+        }
+
+        return response()->json([
+            'status'    => true,
+            'message'   => 'Solicitud de registro recuperado exitosamente',
+            'data'      => $declaracionJurada
+        ], 200);
     }
 
     /**
@@ -48,7 +80,26 @@ class DeclaracionJuradaController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $declaracionJurada = DeclaracionJurada::find($id);
+
+        if (is_null($declaracionJurada)) {
+            return response()->json([
+                'status'    => false,
+                'message'   => 'Registro no encontrado'
+            ], 200);
+        }
+        $declaracionJurada->persona_id                       = $request->persona_id;
+        $declaracionJurada->declaracion_jurada               = $request->declaracion_jurada;
+        $declaracionJurada->fecha_inicio                     = $request->fecha_inicio;
+        $declaracionJurada->fecha_fin                        = $request->fecha_fin;
+        $declaracionJurada->vigente                          = $request->vigente;
+        $declaracionJurada->save();
+
+        return response()->json([
+            'status'    => true,
+            'message'   => 'Registro modificado exitosamente',
+            'data'      => $declaracionJurada
+        ], 200);
     }
 
     /**
@@ -59,6 +110,36 @@ class DeclaracionJuradaController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $declaracionJurada = DeclaracionJurada::find($id);
+
+        if (is_null($declaracionJurada)) {
+            return response()->json([
+                'status'    => false,
+                'message'   => 'Solicitud no encontrado'
+            ], 200);
+        }
+        $declaracionJurada->delete();
+        return response()->json([
+            'status'    => true,
+            'message'   => 'Solicitud eliminado exitosamente',
+            'data'      => $declaracionJurada
+        ], 200);
+    }
+
+    public function declaracionJuaradaPersonaId($persona_id)
+    {
+        $declaracionJurada = DeclaracionJurada::where('persona_id', $persona_id)->get();
+        
+        if (is_null($declaracionJurada)) {
+            return response()->json([
+                'status'    => false,
+                'message'   => 'Solicitud de registro no encontrado'
+            ], 200);
+        }
+        return response()->json([
+            'status'    => true,
+            'message'   => 'Solicitud de registro recuperado exitosamente',
+            'data'      => $declaracionJurada
+        ], 200);
     }
 }
