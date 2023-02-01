1dd8bf947449e320c25c1b7d047e57be7f214400
coroneldev <rcoroneldeveloper@gmail.com>
2023-02-01 17:04:19 -0400

    migracion de controladores con rutas

--- app/Models/Rh/Cl/CategoriaViaje.php
+++ app/Models/Rh/Cl/CategoriaViaje.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Categoria extends Model
+class CategoriaViaje extends Model
 {
     use HasFactory;
     use SoftDeletes;
--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,26 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+/*Administracion de sistema*/
 use App\Http\Controllers\Api\Rh\Trn\UserController;
-
 use App\Http\Controllers\Api\Rh\Cl\SistemaController;
 use App\Http\Controllers\Api\Rh\Trn\MenuController;
 use App\Http\Controllers\Api\Rh\Cl\RolController;
 use App\Http\Controllers\Api\Rh\Trn\AccesoController;
+
+/*Recursos Humanos*/
+
+/*Clasificadores*/
+use App\Http\Controllers\Api\Rh\Cl\EstadoCivilController;
+use App\Http\Controllers\Api\Rh\Cl\GeneroController;
+use App\Http\Controllers\Api\Rh\Cl\PaisController;
+use App\Http\Controllers\Api\Rh\Cl\CiudadController;
+use App\Http\Controllers\Api\Rh\Cl\ParentescoController;
+use App\Http\Controllers\Api\Rh\Cl\TipoDocumentoController;
+use App\Http\Controllers\Api\Rh\Cl\InstitucionController;
+use App\Http\Controllers\Api\Rh\Cl\NivelEstudioController;
+use App\Http\Controllers\Api\Rh\Cl\EstadoController;
+use App\Http\Controllers\Api\Rh\Cl\CategoriaViajeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -61,7 +75,71 @@ Route::prefix('sys')->group(function () {
 
 Route::prefix('rh')->group(function () {
 
+    Route::get('/estados-civiles', [EstadoCivilController::class, 'index']);
+    Route::post('/estados-civiles', [EstadoCivilController::class, 'store']);
+    Route::get('/estados-civiles/{id}', [EstadoCivilController::class, 'show']);
+    Route::put('/estados-civiles/{id}', [EstadoCivilController::class, 'update']);
+    Route::delete('/estados-civiles/{id}', [EstadoCivilController::class, 'destroy']);
+    
+    Route::get('/generos', [GeneroController::class, 'index']);
+    Route::post('/generos', [GeneroController::class, 'store']);
+    Route::get('/generos/{id}', [GeneroController::class, 'show']);
+    Route::put('/generos/{id}', [GeneroController::class, 'update']);
+    Route::delete('/generos/{id}', [GeneroController::class, 'destroy']);
+
+    Route::get('/paises', [PaisController::class, 'index']);
+    Route::post('/paises', [PaisController::class, 'store']);
+    Route::get('/paises/{id}', [PaisController::class, 'show']);
+    Route::put('/paises/{id}', [PaisController::class, 'update']);
+    Route::delete('/paises/{id}', [PaisController::class, 'destroy']);
+    
+    Route::get('/ciudades', [CiudadController::class, 'index']);
+    Route::post('/ciudades', [CiudadController::class, 'store']);
+    Route::get('/ciudades/{id}', [CiudadController::class, 'show']);
+    Route::put('/ciudades/{id}', [CiudadController::class, 'update']);
+    Route::delete('/ciudades/{id}', [CiudadController::class, 'destroy']);
+
+    Route::get('/parentescos', [ParentescoController::class, 'index']);
+    Route::post('/parentescos', [ParentescoController::class, 'store']);
+    Route::get('/parentescos/{id}', [ParentescoController::class, 'show']);
+    Route::put('/parentescos/{id}', [ParentescoController::class, 'update']);
+    Route::delete('/parentescos/{id}', [ParentescoController::class, 'destroy']);
+
+    Route::get('/tipos-documentos', [TipoDocumentoController::class, 'index']);
+    Route::post('/tipos-documentos', [TipoDocumentoController::class, 'store']);
+    Route::get('/tipos-documentos/{id}', [TipoDocumentoController::class, 'show']);
+    Route::put('/tipos-documentos/{id}', [TipoDocumentoController::class, 'update']);
+    Route::delete('/tipos-documentos/{id}', [TipoDocumentoController::class, 'destroy']);
+
+    Route::get('/instituciones', [InstitucionController::class, 'index']);
+    Route::post('/instituciones', [InstitucionController::class, 'store']);
+    Route::get('/instituciones/{id}', [InstitucionController::class, 'show']);
+    Route::put('/instituciones/{id}', [InstitucionController::class, 'update']);
+    Route::delete('/instituciones/{id}', [InstitucionController::class, 'destroy']);
+
+    Route::get('/niveles-estudios', [NivelEstudioController::class, 'index']);
+    Route::post('/niveles-estudios', [NivelEstudioController::class, 'store']);
+    Route::get('/niveles-estudios/{id}', [NivelEstudioController::class, 'show']);
+    Route::put('/niveles-estudios/{id}', [NivelEstudioController::class, 'update']);
+    Route::delete('/niveles-estudios/{id}', [NivelEstudioController::class, 'destroy']);
+
+    Route::get('/estados', [EstadoController::class, 'index']);
+    Route::post('/estados', [EstadoController::class, 'store']);
+    Route::get('/estados/{id}', [EstadoController::class, 'show']);
+    Route::put('/estados/{id}', [EstadoController::class, 'update']);
+    Route::delete('/estados/{id}', [EstadoController::class, 'destroy']);
+    Route::get('/estados/seccion/{seccion_id}', [EstadoController::class, 'estadosPorSeccion']);
+
+    Route::get('/catetgorias-viaje', [CategoriaViajeController::class, 'index']);
+    Route::post('/catetgorias-viaje', [CategoriaViajeController::class, 'store']);
+    Route::get('/catetgorias-viaje/{id}', [CategoriaViajeController::class, 'show']);
+    Route::put('/catetgorias-viaje/{id}', [CategoriaViajeController::class, 'update']);
+    Route::delete('/catetgorias-viaje/{id}', [CategoriaViajeController::class, 'destroy']);
+
+
+
 });
+
 Route::prefix('pv')->group(function () {
 
 });
